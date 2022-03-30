0dabcc5e30820684d6439f12beb30725bba9b5cb
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-30 20:03:20 -0300

    Feat: salesServices getById

--- services/salesServices.js
+++ services/salesServices.js
@@ -9,6 +9,16 @@ const getAll = async () => {
   return orderedArray;
 };
 
+const getById = async (id) => {
+  const sale = await salesModel.getById(id);
+
+  if (!sale) {
+    throw new Error('SALE_NOT_FOUND');
+  }
+
+  return sale;
+};
+
 module.exports = {
   getAll,
   getById,
