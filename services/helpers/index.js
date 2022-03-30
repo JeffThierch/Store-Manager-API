9a8c7b49d98bf66c1fb5db45923e24d1d646a228
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-30 19:04:04 -0300

    Feat: ProductServices getAll method

---
+++ services/helpers/index.js
@@ -0,0 +1,15 @@
+const orderArrayOfObjectByProperty = (array, property) => array.sort((a, b) => {
+    if (a[property] < b[property]) {
+      return -1;
+    }
+
+    if (a[property] > b[property]) {
+      return 1;
+    }
+
+    return 0;
+});
+
+module.exports = {
+  orderArrayOfObjectByProperty,
+};
--- services/productsServices.js
+++ services/productsServices.js
@@ -1,1 +1,14 @@
-// Renomeie esse arquivo
+const productsModel = require('../models/ProductsModel');
+const { orderArrayOfObjectByProperty } = require('./helpers');
+
+const getAll = async () => {
+  const products = await productsModel.getAll();
+
+  const orderedArray = orderArrayOfObjectByProperty(products, 'id');
+
+  return orderedArray;
+};
+
+module.exports = {
+  getAll,
+};
