8b951d8538568f617031127df63fc55c0492fbc2
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-31 17:50:33 -0300

    Fix: salesModel Query

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -1,18 +1,33 @@
 const connection = require('./connection');
 
-const serialize = (saleData) => ({
-  saleId: saleData.sale_id,
-  date: saleData.date,
-  productId: saleData.product_id,
-  quantity: saleData.quantity,
-});
+const serialize = (saleData, withId = true) => {
+  if (withId) {
+    return {
+      saleId: saleData.sale_id,
+      date: saleData.date,
+      productId: saleData.product_id,
+      quantity: saleData.quantity,
+    };
+  }
+
+  return {
+    date: saleData.date,
+    productId: saleData.product_id,
+    quantity: saleData.quantity,
+  };
+};
 
 const getAll = async () => {
-  const query = 'SELECT * FROM StoreManager.sales_products ORDER BY sale_id ASC, product_id ASC;';
+  const query = `
+    SELECT sp.sale_id, s.date, sp.product_id, sp.quantity
+    FROM StoreManager.sales_products AS sp
+    LEFT JOIN sales AS s
+    ON s.id = sp.sale_id
+    ORDER BY sale_id ASC, product_id ASC;`;
 
   const [sales] = await connection.execute(query);
 
-  const serializedData = sales.map(serialize);
+  const serializedData = sales.map((sale) => serialize(sale, true));
   return serializedData;
 };
 
@@ -21,14 +36,15 @@ const getById = async (id) => {
     SELECT sp.sale_id, s.date, sp.product_id, sp.quantity
     FROM StoreManager.sales_products AS sp
     LEFT JOIN sales AS s ON s.id = sp.sale_id
+    WHERE sp.sale_id = ?
     ORDER BY sp.sale_id ASC, sp.product_id ASC;
   `;
 
-  const [sale] = await connection.execute(query, [id]);
+  const [sales] = await connection.execute(query, [id]);
 
-  if (!sale.length) return false;
+  if (!sales.length) return false;
 
-  const serializedData = serialize(sale[0]);
+  const serializedData = sales.map((sale) => serialize(sale, false));
 
   return serializedData;
 };
