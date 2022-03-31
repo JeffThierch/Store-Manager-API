1479a32202439168938a2215f6efacc3e232f575
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-31 16:13:40 -0300

    Feat: salesController getById method

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -6,6 +6,19 @@ const getAll = async (_req, res) => {
   return res.status(200).json(sales);
 };
 
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const sale = await salesServices.getById(id);
+
+    return res.status(200).json(sale);
+  } catch (err) {
+    return next(err.message);
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };
