b141cdfb1b8e90bd78998678929a934f662395da
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-31 16:54:25 -0300

    Feat: sales getAll and getById routes

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -3,4 +3,8 @@ const salesController = require('../controllers/salesController');
 
 const router = express.Router();
 
+router
+  .get('/', salesController.getAll)
+  .get('/:id', salesController.getById);
+
 module.exports = router;
