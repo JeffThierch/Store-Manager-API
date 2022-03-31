b2d949e9ae9bc3b28ba88cf52052d15a0f450b8c
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-03-31 14:40:43 -0300

    Feat: ProductController getAll

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,1 +1,11 @@
-// Renomeie esse arquivo
+const productsServices = require('../services/productsServices');
+
+const getAll = async (_req, res) => {
+  const products = productsServices.getAll();
+
+  return res.status(200).json(products);
+};
+
+module.exports = {
+  getAll,
+};
